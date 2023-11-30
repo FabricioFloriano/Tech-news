e67a8c6cabb60115cc4d0ad6fcfecd5bc296bc47
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-30 11:12:18 -0300

    Requisito 10? - Criada  a função top_5_categories

--- tech_news/analyzer/ratings.py
+++ tech_news/analyzer/ratings.py
@@ -1,4 +1,19 @@
 # Requisito 10
+from tech_news.database import find_news
+
+
 def top_5_categories():
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    all_news = find_news()
+
+    category_counts = {}
+    for news in all_news:
+        category = news["category"]
+        category_counts[category] = category_counts.get(category, 0) + 1
+
+    sorted_categories = sorted(
+        category_counts.items(), key=lambda x: (-x[1], x[0])
+    )
+
+    top_5 = [category for category, _ in sorted_categories[:5]]
+
+    return top_5
