17a05329e1498bdbf41a69f97f77607d0a5c1ac1
Fabricio Floriano <f.florian0@hotmail.com>
2023-11-30 10:00:42 -0300

    Requisito 08? - Criada a função search_by_date

--- tech_news/analyzer/search_engine.py
+++ tech_news/analyzer/search_engine.py
@@ -1,4 +1,6 @@
 # Requisito 7
+
+from datetime import datetime
 from tech_news.database import search_news
 
 
@@ -11,8 +13,17 @@ def search_by_title(title):
 
 # Requisito 8
 def search_by_date(date):
-    """Seu código deve vir aqui"""
-    raise NotImplementedError
+    try:
+        date_format = datetime.strptime(
+            date, "%Y-%m-%d").strftime("%d/%m/%Y")
+    except ValueError:
+        raise ValueError("Data inválida")
+
+    response = []
+    news = search_news({"timestamp": date_format})
+    for new in news:
+        response.append((new["title"], new["url"]))
+    return response
 
 
 # Requisito 9
